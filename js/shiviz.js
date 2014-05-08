4619947bb02c62ef2f77049a21ca660a4f9ce78f
olzhang <oliver.zhang2718@gmail.com>
2014-05-07 23:46:29 -0700

    New branch for final documentation

--- js/shiviz.js
+++ js/shiviz.js
@@ -10,6 +10,13 @@ $("#logField").on('input propertychange', function(e) {
 
 get("versionContainer").innerHTML = versionText;
 
+// variables to store last node in a process
+var lastNodesElements = {};
+// variable to store original colours
+var hostColors = {};
+//variables to store the id attached to each process box 
+var hosts = {};
+
 function resetView() {
   // Enable/disable the visualize button depending on whether or not
   // the text area is empty.
@@ -36,9 +43,120 @@ get("vizButton").onclick = function() {
   var lines = textBox.value.split('\n');
 
   var view = new View(generateGraphFromLog(lines));
+  console.log(view.getHostColors());
+  hostColors = view.getHostColors(); 
+  hosts = view.hosts;
+  lastNodesId = view.getLastNodeId();
+  
   get("graph").hidden = false;
   view.draw();
+  
+  createLastNodeElements();
+  
+  /*
+  The following snippet of code is an attempt to address issue #14: Grey out process boxes 
+  once their events have been scrolled through. The code checks to see if the final node in 
+  the process is in view. If it is, then it greys out the process box, else it does not. 
+  It is commented out because the code generates the following bugs:
+	1. Once the boxes are greyed out, scrolling up would cause all the boxes to be coloured in at once.
+	2. The boxes do not get greyed out once they are immediately scrolled out of view. You have to scroll down one more time to see it greyed out
+	You can see the above two bugs here: http://olzhang.bitbucket.org/shiviz.html. 
+	Source code here: https://bitbucket.org/olzhang/olzhang.bitbucket.org
+	
+	The .bind("inview",  function(event, isInView, visiblePartX, visiblePartY) {…} is not a 
+	native jquery method. It is made possible by the jquery.inview 
+	library (https://github.com/protonet/jquery.inview) (not included here). 
+	The following snippet of code uses this inview method, which returns a boolean value to detect 
+	whether an element of the DOM is visible to the user. Returns false if the object is not viewable; 
+	true if it is.    
+
+	For easy debugging, you can type in hostColors into the console to see the colours 
+	associated with each process box, or lastNodesElement to see the final node 
+	associated with the process, and hosts to see the hostId ossociated with each process.
+	*/   
+
+
+  
+  /*
+  for(n in lastNodesElements) {
+  	// binds the inview event to each final node 
+  	$(lastNodesElements[n]).bind("inview",  function(event, isInView, visiblePartX, visiblePartY) {
+  		
+  		
+  		var matchingHostId = $(this).find('circle').attr('id');
+  		// matches each last node with the corresponding host 
+  		var matchingHost = $("rect").filter(function() {
+  			if($(this).attr("id")==matchingHostId) {
+  				return $(this);
+  			}
+  		});
+  		var lastScrollTop = 0;
+  		$(window).scroll(function(event){
+  			var st = $(this).scrollTop();
+  			if (st > lastScrollTop) {
+  				// downscroll code
+  				if(!isInView) {
+  					// if you are scrolling down and the final node is out of view 
+  					// grey out the host (i.e. process box) 
+  					$(matchingHost).attr("style", "stroke: #ffffff; fill: #cdcdcd;");
+  				} else {
+  					// if you are scrolling down and the final node is not out of view, they keep
+  					// the original colour of the process box  
+  					var k;
+  					for(var c in hostColors) {
+  						if(c==matchingHostId)
+  							k = hostColors[c]; 
+  					}
+  					$(matchingHost).attr("style", "stroke: #ffffff; fill: "+k+";");
+  				}
+  			} else {
+  				if(!isInView || ($(matchingHost).css("fill")=="#cdcdcd")) {
+  					// if you are scrolling up and the final node is not inview and the 
+  					// host (i.e. the process box) is greyed out then keep it greyed out  
+  					$(matchingHost).attr("style", "stroke: #ffffff; fill: #cdcdcd;");
+  				} else {
+  					// if you are scrolling up and the final node is not out of view, then keep
+  					// the original colour of the process box 
+  					var k;
+  					for(var c in hostColors) {
+  						if(c==matchingHostId)
+  							k = hostColors[c]; 
+  					}
+  					$(matchingHost).attr("style", "stroke: #ffffff; fill: "+k+";");
+  				}
+  			}
+  			lastScrollTop = st;
+  		});
+  	});
+  }
+  */
 };
+
+/**
+ * returns the last node associated with a certain process id
+ * @param String hostid
+ */
+function getLastNodeElementOfHost(hostid) {
+	var htmlElem;
+	htmlElem = $("g").filter(function () {
+		if($(this).find("title").text()==lastNodesId[hostid]["logEvent"]) {
+			return this;
+		} 
+	});
+	return htmlElem;
+};
+
+/**
+ * creates an object with the process (host) id as the key and the 
+ * last node associated with the process as the value 
+ */
+function createLastNodeElements() {
+	lastNodesElements = {};
+	for (var i=0; i<hosts.length; i++) {
+		lastNodesElements[hosts[i]] = getLastNodeElementOfHost(hosts[i]);
+	}
+}
+
 
 function handleLogFileResponse(response, linkObj) {
   get("logField").value = response;
