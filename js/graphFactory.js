f2293ff8dbc67b671d7501e998a7a29f85cdbd42
pqperry <weba1.pq@gmail.com>
2014-05-15 19:39:01 -0700

    Improved error handling.
    -- Bad regexes now give a more descriptive error message
    -- If none of the regexes match a specific log line, an error is now thrown instead of failing silently
    -- An error is now shown if you try to use a reserved capture group name

--- js/graphFactory.js
+++ js/graphFactory.js
@@ -13,7 +13,15 @@ function generateGraphFromLog(logLines, regex) {
     return false;
   }
   
-  var myRegexes = parseRegexes(regex);
+  var myRegexes = null; 
+  try {
+    myRegexes = parseRegexes(regex);
+  }
+  catch (err) {
+    alert(err);
+    resetView();
+    return null;
+  }
 
   model = new Graph();
 
@@ -31,11 +39,16 @@ function generateGraphFromLog(logLines, regex) {
       var clock = JSON.parse(stamp.substring(spacer));
       
       var data = parseLogText(log, myRegexes);
+      if(data == null) {
+        alert("Log line " + (i + 1) + " cannot be matched by any regex.\n\n" + log);
+        resetView();
+        return null;
+      }
 
       model.addNode(new Node(log + " " + JSON.stringify(data), host, clock, i, data));
     }
   }catch (err) {
-    alert("Error parsing input, malformed logs: " + i);
+    alert("Error parsing input, malformed logs on line: " + i);
     resetView();
     return null;
   }
@@ -59,31 +72,59 @@ function MyRegex(string, groupNumToName) {
 /**
  * Parses regex from raw textfield input. Each regex must be entered on its own line.
  * This function abstracts away named capture groups into numbered groups and a mapping of
- * numbers to names. This function does not check if the regexes are correct or not.
+ * numbers to names. This function checks if the regex is valid and throws an error if not.
+ * This function ignores blank lines in between regexes in the raw string
  * @param regex	The raw textfield input
  * @returns {Array}	An array of MyRegex objects.
+ * @throws Descriptive string if an error occurs during parsing.
  */
 function parseRegexes(regex) {
+  var reservedCaptureGroups = ["text", "matchedText"];
+  var isReserved = {};
+  for(var i = 0; i < reservedCaptureGroups.length; i++) {
+    isReserved[reservedCaptureGroups[i]] = true;
+  }
+  
 	// if no regex entered, provide default one that captures everything
 	if(regex.trim().length == 0) {
-		regex = "(?<text>.*)";
+		regex = ".*";
 	}
 	
+	var captureGroupStart = /\(\?<(\w+?)>/g;
 	var ret = [];
+	var isReservedNameError = false;
+	
 	var regexArray = regex.split("\n");
 	for(var i = 0; i < regexArray.length; i++) {
-		var curr = regexArray[i];
-		var captureGroupStart = /\(\?<(.+?)>/g;
-		var groupNumToName = [];
-		var match = null;
-		while((match = captureGroupStart.exec(curr)) != null) {
-			groupNumToName.push(match[1]);
-		}
-		
-		var nonCaptureGroupStart = /\((?!\?)/g;
-		curr = curr.replace(nonCaptureGroupStart, "(?:").replace(captureGroupStart, "(");
-
-		ret.push(new MyRegex(curr, groupNumToName));
+  		var curr = regexArray[i];
+  		if(curr.trim().length == 0) {
+  		  continue;
+  		}
+  		
+  	try {
+  		var groupNumToName = [];
+  		var match = null;
+  		while((match = captureGroupStart.exec(curr)) != null) {
+  		  var name = match[1];
+        if(isReserved[name]) {
+          isReservedNameError = true;
+          throw "Error. \"" + name + "\" is a reserved capture group name.\nRegex on line " + (i+1) + ":\n" + curr;
+        }
+  			groupNumToName.push(name);
+  		}
+  		
+  		var nonCaptureGroupStart = /\((?!\?)/g;
+  		var finalRegex = curr.replace(nonCaptureGroupStart, "(?:").replace(captureGroupStart, "(");
+  		new RegExp(finalRegex); // this ensures the regex is valid
+  
+  		ret.push(new MyRegex(finalRegex, groupNumToName));
+	  }
+	  catch (err) {
+	    if(isReservedNameError) {
+	      throw err;
+	    }
+	    throw "Invalid regex on line " + (i+1) + ":\n" + curr;
+	  }
 	}
 	
 	return ret;
@@ -93,10 +134,9 @@ function parseRegexes(regex) {
  * Extracts data from log text. The first regex in Regexes that matches text is used.
  * @param text	The string on which we perform regex searches
  * @param myRegexes	Array of MyRegex.
- * @returns	An Object of key-value pairs of data extracted from log text. object[x] == y such 
- * that y is the value associated with capture group x. 
+ * @returns	An Object of key-value pairs of data extracted from log text. object[x] == y such
+ * that y is the value associated with capture group x. If no regexes match text, returns null;
  * "Default" capture groups are included. (e.g. object["text"] = full text of the log).
- * "Default" capture groups are included even if none of the provided regexes match
  */
 function parseLogText(text, myRegexes) {
 	var ret = {};
@@ -111,10 +151,10 @@ function parseLogText(text, myRegexes) {
 		for(var j = 1; j < execResult.length; j++) {
 			ret[myRegexes[i].groupNumToName[j-1]] = execResult[j];
 		}
-		break;
+		return ret;
 	}
 	
-	return ret;
+	return null;
 }
 
 
