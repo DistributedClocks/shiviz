ce507449f94c74c5223bceb20d94699e8b238299
Albert Xing <albert.xing@alumni.ubc.ca>
2014-05-26 09:06:38 -0700

    Change development log location to kay.cs.ubc.ca

--- js/dev.js
+++ js/dev.js
@@ -1,3 +1,3 @@
 // Where to load example logs from:
-var logUrlPrefix='http://www.corsproxy.com/bestchai.bitbucket.org/shiviz/';
+var logUrlPrefix='http://kay.cs.ubc.ca/';
 var versionText='Development version.'
