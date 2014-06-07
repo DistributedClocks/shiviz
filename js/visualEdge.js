3fac415b4db412a352b46615c64787a28b2b406d
pqperry <weba1.pq@gmail.com>
2014-06-06 21:30:07 -0700

    Transitive edges now drawn with dashed lines

--- js/visualEdge.js
+++ js/visualEdge.js
@@ -3,6 +3,7 @@ function VisualEdge(sourceVisualNode, targetVisualNode) {
     this.sourceVisualNode = sourceVisualNode;
     this.targetVisualNode = targetVisualNode;
     this.width = 1;
+    this.dashLength = 0;
 }
 
 VisualEdge.prototype.getSourceVisualNode = function() {
@@ -19,4 +20,12 @@ VisualEdge.prototype.getWidth = function() {
 
 VisualEdge.prototype.setWidth = function(newWidth) {
     this.width = newWidth;
+};
+
+VisualEdge.prototype.getDashLength = function() {
+    return this.dashLength;
+};
+
+VisualEdge.prototype.setDashLength = function(newDashLength) {
+    this.dashLength = newDashLength; //todo: validate
 };
