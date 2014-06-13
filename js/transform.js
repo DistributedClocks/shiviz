49a2a238d0f130925333fb1d5b6b0071ebdbe91b
pqperry <weba1.pq@gmail.com>
2014-06-13 16:46:54 -0700

    Even more comments still

--- js/transform.js
+++ js/transform.js
@@ -11,8 +11,9 @@
  */
 
 /**
- * Transformation to hide a host from the model. Adds transitive edges to the
- * model.
+ * This transformation generates a transformed model by removing this Transformation's hostToHide
+ * from the provided model. It removes all nodes for the hostToHide and any edges
+ * touching a node for the hostToHide and adds transitive edges. The added transitive edges will be drawn with dashed lines.
  * 
  * @constructor
  * @param {String} hostToHide The host to hide from the model
@@ -26,14 +27,8 @@ function HideHostTransformation(hostToHide) {
 }
 
 /**
- * @class
- * Generates a transformed model by removing this Transformation's hostToHide
- * from the provided model. Removes all nodes for the hostToHide and any edges
- * touching a node for the hostToHide and adds transitive edges. The added transitive edges will be drawn with dashed lines.
- * This method
- * modifies the provided graph in place
- * 
- * @param {Graph} graph The graph to transform. Modified in place
+ * Performs the transformation on the given visualGraph. The VisualGraph and its underlying Graph are modified in place
+ * @param {VisualGraph} visualGraph The VisualGraph to transform
  */
 HideHostTransformation.prototype.transform = function(visualGraph) {
 
@@ -79,7 +74,11 @@ HideHostTransformation.prototype.transform = function(visualGraph) {
  * @class
  * CollapseSequentialNodeTransformation groups local consecutive events that have no remote dependencies. The collapsed nodes
  * will have an increased radius and will contain a label indicating the number of nodes collapsed into it. This transformation
- * provides methods for adding and removing nodes exempt from this collapsing process
+ * provides methods for adding and removing nodes exempt from this collapsing process.
+ * 
+ * This transformation collapses nodes that belong to the same group. Intuitively, nodes belong to the same
+ * group if they are local consecutive events that have no remote dependencies. More formally, a node y is in x's group
+ * if y == x or y has no family and y's prev or next node is in x's group.
  * 
  * @param {Number} threshold Nodes are collapsed if the number of nodes in the group is greater than or equal to the threshold. The threshold must be greater than or equal to 2.
  * @returns
@@ -118,7 +117,14 @@ CollapseSequentialNodesTransformation.prototype.setThreshold = function(threshol
     this.threshold = threshold;
 };
 
-
+/**
+ * Adds an exemption. An exemption is a LogEvent whose Node will never be collapsed. 
+ * 
+ * Note that addExemption and removeExemption are not inverses of each other. addExemption affects only the 
+ * LogEvents of the given node, while removeExemption affects the LogEvents of the given node and all nodes in its group.
+ * 
+ * @param {Node} node The node whose LogEvents will be added as exemptions
+ */
 CollapseSequentialNodesTransformation.prototype.addExemption = function(node) {
     var logEvents = node.getLogEvents();
     for(var i = 0; i < logEvents.length; i++) {
@@ -126,8 +132,21 @@ CollapseSequentialNodesTransformation.prototype.addExemption = function(node) {
     }
 };
 
+
+/**
+ * Removes an exemption. An exemption is a LogEvent whose Node will never be collapsed
+ * 
+ * Note that addExemption and removeExemption are not inverses of each other. addExemption affects only the 
+ * LogEvents of the given node, while removeExemption affects the LogEvents of the given node and all nodes in its group.
+ * 
+ * @param {Node} node The LogEvents of this node and the LogEvents of every node in its group will be removed as exemptions
+ */
 CollapseSequentialNodesTransformation.prototype.removeExemption = function(node) {
     if(node.hasChildren() || node.hasParents()) {
+        var logEvents = node.getLogEvents();
+        for(var i = 0; i < logEvents.length; i++) {
+            delete this.exemptLogEvents[logEvents[i].getId()];
+        }
         return;
     }
     
@@ -160,6 +179,12 @@ CollapseSequentialNodesTransformation.prototype.toggleExemption = function(node)
     }
 };
 
+/**
+ * Determines if any of the LogEvents contained inside the given node is an exemption
+ * 
+ * @param {Node} node The node to check
+ * @returns {Boolean} True if one of the LogEvents is an exemption
+ */
 CollapseSequentialNodesTransformation.prototype.isExempt = function(node) {
     var logEvents = node.getLogEvents();
     for(var i = 0; i < logEvents.length; i++) {
@@ -170,7 +195,10 @@ CollapseSequentialNodesTransformation.prototype.isExempt = function(node) {
     return false;
 };
 
-
+/**
+ * Performs the transformation on the given visualGraph. The VisualGraph and its underlying Graph are modified in place
+ * @param {VisualGraph} visualGraph The VisualGraph to transform
+ */
 CollapseSequentialNodesTransformation.prototype.transform = function(visualGraph) {
 
     var graph = visualGraph.getGraph();
