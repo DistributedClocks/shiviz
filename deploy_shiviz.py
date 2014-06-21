b28a091d8b1983bc3ee2bb47900b75c870a49bc5
Albert Xing <albert.xing@alumni.ubc.ca>
2014-06-20 17:06:59 -0700

    Add minification to deploy script

--- deploy_shiviz.py
+++ deploy_shiviz.py
@@ -29,6 +29,8 @@ Usage:
 
 import sys
 import os
+import httplib
+import urllib
 import subprocess
 
 
@@ -83,19 +85,71 @@ def main():
     # Remove the unnecessary dev.js that was copied over.
     runcmd("rm " + dist_dir + "js/dev.js")
 
-    # Replace reference to dev.js with deployed.js in deployed version
-    # of index.html.
-    runcmd("sed -i '' 's/dev.js/deployed.js/g' " + dist_dir + "index.html")
-
     # Find out the current revision id:
     revid = get_cmd_output('hg', ['id', '-i']);
     revid = revid.rstrip()
 
-    print "Revid is : " + revid;
+    print "Revid is : " + revid
 
     # Replace the place-holder revision with the actual revision id:
     runcmd("sed -i '' 's/revision: ZZZ/revision: " + revid
            + "/g' " + dist_dir + "js/deployed.js")
+
+    # Minify the code
+    print "Minifying... please wait"
+
+    params = [
+    ('code_url', 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js'),
+    ('code_url', 'http://d3js.org/d3.v3.min.js'),
+    ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
+    ('output_format', 'text'),
+    ('output_info', 'compiled_code')
+    ]
+
+    files = os.listdir('./js')
+    for file in files:
+        params += [('code_url', 'https://bitbucket.org/bestchai/shiviz/raw/tip/js/' + file)]
+
+    urlparams = urllib.urlencode(params)
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
+    conn = httplib.HTTPConnection('closure-compiler.appspot.com')
+    conn.request('POST', '/compile', urlparams, headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    if data.length < 500:
+        print "Minification failed!"
+        params = [
+        ('code_url', 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js'),
+        ('code_url', 'http://d3js.org/d3.v3.min.js'),
+        ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
+        ('output_format', 'text'),
+        ('output_info', 'errors')
+        ]
+
+        files = os.listdir('./js')
+        for file in files:
+            params += [('code_url', 'https://bitbucket.org/bestchai/shiviz/raw/tip/js/' + file)]
+
+            urlparams = urllib.urlencode(params)
+            headers = {'Content-type': 'application/x-www-form-urlencoded'}
+            conn = httplib.HTTPConnection('closure-compiler.appspot.com')
+            conn.request('POST', '/compile', urlparams, headers)
+            response = conn.getresponse()
+            data = response.read()
+
+        print data
+
+    else:
+        minified = open('js/min.js', 'w')
+        minified.write(data)
+
+        # Replace reference to js files with minified js in deployed version
+        # of index.html.
+        runcmd("sed -i '' 's/<script[^>]*><\/script>//g' " + dist_dir + "index.html")
+        runcmd("sed -i '' 's/<\/body>/<script src=\"js\/min.js\"><\/script>/g' " + dist_dir + "index.html")
+
+        print "Minification successful!"
 
     # Remove any files containing '#'
     runcmd("cd " + dist_dir + " && find . | grep '#' | xargs rm")
