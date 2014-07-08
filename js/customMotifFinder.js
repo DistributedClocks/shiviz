b96d197476c29800c3e74237de937203be4937d6
Perry Liu <weba1.pq@gmail.com>
2014-07-07 17:37:44 -0700

    More code

--- js/customMotifFinder.js
+++ js/customMotifFinder.js
@@ -5,159 +5,170 @@ function CustomMotifFinder(builderGraph) {
 CustomMotifFinder.prototype.find = function(graph) {
     var context = this;
     var builderGraph = this.builderGraph;
-    
-    var hostMatch = {}; // graph host to builderGraph host
+
+    // we don't need host tracking, since prev/next connectivity constrains host
+    // already
+    // var hostMatch = {}; // graph host to builderGraph host
     var nodeMatch = {}; // node to builderNode
-    var bNodeMatch = {}; //builderNode to node
-    var hostFixCount = {}; // graph host to count
-    
+    var bNodeMatch = {}; // builderNode to node
+    // var hostFixCount = {}; // graph host to count
+    var used = {}; // nodes already part of other motifs
+
+    var motif = new Motif();
+
     var startBuilderNode = builderGraph.getNodes()[0];
     var nodes = graph.getNodes();
-    for(var n = 0; n < nodes.length; n++) {
-        var node = nodes[n];
-        setNodeMatch(startBuilderNode, node);
-        if(searchNode(startBuilderNode)) {
-            console.log("FOUND!!!"); //TODO replace
-            var motif = new Motif();
-            for(var key in bNodeMatch) {
-                motif.addNode(bNodeMatch[key]);
-            }
-            return motif;
+    for ( var n = 0; n < nodes.length; n++) {
+        var cnode = nodes[n];
+
+        setNodeMatch(startBuilderNode, cnode);
+        if (searchNode(startBuilderNode)) {
+            handleFound();
         }
-        removeNodeMatch(node);
+        removeNodeMatch(cnode);
     }
-    console.log("find fail");
-    
+
+    return motif;
+
+    function handleFound() {
+
+        for ( var key in bNodeMatch) {
+            var nodeToAdd = bNodeMatch[key];
+            motif.addNode(nodeToAdd);
+            used[nodeToAdd.getId()] = true;
+        }
+
+        hostMatch = {}; // graph host to builderGraph host
+        nodeMatch = {}; // node to builderNode
+        bNodeMatch = {}; // builderNode to node
+        hostFixCount = {}; // graph host to count
+    }
+
     function searchNode(builderNode) {
         var node = bNodeMatch[builderNode.getId()];
-        
-//        if(builderNode.getNext() != null && !builderNode.getNext().isTail()) {
-//            if(node.getNext() == null || node.getNext().isTail()) {
-//                return false;
-//            }
-//            else if(!tryAssign([builderNode.getNext()], [node.getNext()])) {
-////                console.log(builderNode.getNext(), node.getNext());
-//                return false;
-//            }
-//        }
-//        
-//        if(builderNode.getPrev() != null && !builderNode.getPrev().isHead()) {
-//            if(node.getPrev() == null || node.getPrev().isHead()) {
-//                return false;
-//            }
-//            else if(!tryAssign([builderNode.getPrev()], [node.getPrev()])) {
-//                return false;
-//            }
-//        }
-        
-        
-        
-        return tryAssign(builderNode.getChildren(), node.getChildren())
-        || tryAssign(builderNode.getParents(), node.getParents());
-//        || tryAssign([builderNode.getNext()], [node.getNext()])
-//        || tryAssign([builderNode.getPrev()], [node.getPrev()]);
-    }
-    
-    function tryAssign(bNodeGroup, nodeGroup) {
-        
-        // if a bnode has an assignment not in nodegroup, fail
-        var nodeGroupSet = {};
-        for(var i = 0; i < nodeGroup.length; i++) {
-            nodeGroupSet[nodeGroup[i].getId()] = true;
-        }
-        
-        var freeBNodeGroup = [];
-        for(var i = 0; i < bNodeGroup.length; i++) {
-            var match = bNodeMatch[bNodeGroup[i].getId()];
-            if(match == null) {
-                freeBNodeGroup.push(bNodeGroup[i]);
-            }
-            else if(!nodeGroupSet[match.getId()]) {
-                return false;
-            }
-        }
-        
-        // if node has assignment not in bnodegroup, fail
-        var bNodeGroupSet = {};
-        for(var i = 0; i < bNodeGroup.length; i++) {
-            bNodeGroupSet[bNodeGroup[i].getId()] = true;
-        }
-        
-        var freeNodeGroup = [];
-        for(var i = 0; i < nodeGroup.length; i ++) {
-            var match = nodeMatch[nodeGroup[i].getId()];
-            if(!match) {
-                freeNodeGroup.push(nodeGroup[i]);
-            }
-            else if(!bNodeGroupSet[match.getId()]){
-                return false;
-            }
-        }
-        
-        if(freeNodeGroup.length < freeBNodeGroup.length) {
+
+        if (used[node.getId()]) {
             return false;
         }
-        
-        if(freeBNodeGroup.length == 0) {
+
+        if (!builderNode.getNext().isTail() && (node.getNext().isTail() || !tryAssign([ builderNode.getNext() ], [ node.getNext() ]))) {
+            return false;
+        }
+
+        if (!builderNode.getPrev().isHead() && (node.getPrev().isHead() || !tryAssign([ builderNode.getPrev() ], [ node.getPrev() ]))) {
+            return false;
+        }
+
+        return tryAssign(builderNode.getChildren(), node.getChildren()) && tryAssign(builderNode.getParents(), node.getParents());
+    }
+
+    function tryAssign(bNodeGroup, nodeGroup) {
+
+        var newNodeGroup = [];
+        for ( var i = 0; i < nodeGroup.length; i++) {
+            if (!used[nodeGroup[i].getId()]) {
+                newNodeGroup.push(nodeGroup[i]);
+            }
+        }
+        nodeGroup = newNodeGroup;
+
+        // if a bnode has an assignment not in nodegroup, fail
+        var nodeGroupSet = {};
+        for ( var i = 0; i < nodeGroup.length; i++) {
+            nodeGroupSet[nodeGroup[i].getId()] = true;
+        }
+
+        var freeBNodeGroup = [];
+        for ( var i = 0; i < bNodeGroup.length; i++) {
+            var match = bNodeMatch[bNodeGroup[i].getId()];
+            if (match == null) {
+                freeBNodeGroup.push(bNodeGroup[i]);
+            }
+            else if (!nodeGroupSet[match.getId()]) { // TODO check
+                return false;
+            }
+        }
+
+        // if node has assignment not in bnodegroup, fail
+        var bNodeGroupSet = {};
+        for ( var i = 0; i < bNodeGroup.length; i++) {
+            bNodeGroupSet[bNodeGroup[i].getId()] = true;
+        }
+
+        var freeNodeGroup = [];
+        for ( var i = 0; i < nodeGroup.length; i++) {
+            var match = nodeMatch[nodeGroup[i].getId()];
+            if (!match) {
+                freeNodeGroup.push(nodeGroup[i]);
+            }
+            else if (!bNodeGroupSet[match.getId()]) { // TODO check
+                return false;
+            }
+        }
+
+        if (freeNodeGroup.length < freeBNodeGroup.length) {
+            return false;
+        }
+
+        if (freeBNodeGroup.length == 0) {
             return true;
         }
-        
-        tryPermutations(freeNodeGroup, [], [], 0);
-        console.log(freeNodeGroup.length);
-        
+
+        return tryPermutations(freeNodeGroup, [], [], 0);
+
         function tryPermutations(group, perm, taken, nextLoc) {
-            if(nextLoc >= group.length) {
-                for(var i = 0; i < freeBNodeGroup.length; i++) {
-                    if(!searchNode(freeBNodeGroup[i])) {
+            if (nextLoc >= freeBNodeGroup.length) {
+                for ( var i = 0; i < freeBNodeGroup.length; i++) {
+                    if (!searchNode(freeBNodeGroup[i])) {
                         return false;
                     }
                 }
                 return true;
             }
 
-            for(var loc = 0; loc < group.length; loc++) {
-                if(taken[loc]) {
+            for ( var loc = 0; loc < group.length; loc++) {
+                if (taken[loc]) {
                     continue;
                 }
-//                    perm[nextLoc] = group[loc]; //todo: no need?
+
                 var node = group[loc];
-//                console.log(group);
-//                console.log(freeBNodeGroup, nextLoc);
                 setNodeMatch(freeBNodeGroup[nextLoc], node);
                 taken[loc] = true;
-                
-                tryPermutations(group, perm, taken, nextLoc + 1);
-                
+
+                if (tryPermutations(group, perm, taken, nextLoc + 1)) {
+                    return true;
+                }
+
                 taken[loc] = false;
                 removeNodeMatch(node);
             }
             return false;
         }
     }
-    
+
     function setNodeMatch(bNode, node) {
-        if(!!nodeMatch[node.getId()]) {
+        if (!!nodeMatch[node.getId()]) {
             removeNodeMatch(node);
         }
         nodeMatch[node.getId()] = bNode;
         bNodeMatch[bNode.getId()] = node;
-        if(!hostMatch[node.getHost()]) {
-            hostMatch[node.getHost()] = 0;
-        }
-        hostMatch[node.getHost()]++;
+        // if(!hostFixCount[node.getHost()]) {
+        // hostFixCount[node.getHost()] = 0;
+        // }
+        // hostFixCount[node.getHost()]++;
     }
-    
+
     function removeNodeMatch(node) {
-        if(!nodeMatch[node.getId()]) {
+        if (!nodeMatch[node.getId()]) {
             return;
         }
+        delete bNodeMatch[nodeMatch[node.getId()].getId()];
         delete nodeMatch[node.getId()];
-        var host = node.getHost();
-        hostFixCount[host]--;
-        if(hostFixCount[host] == 0) {
-            delete hostMatch[host];
-        }
+        // var host = node.getHost();
+        // hostFixCount[host]--;
+        // if(hostFixCount[host] == 0) {
+        // delete hostMatch[host];
+        // }
     }
-    
-};
 
+};
