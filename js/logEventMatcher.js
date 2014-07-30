0b08b4916de00356a32b86c52c83ad05dbc6bcdd
Perry Liu <aca.perry@gmail.com>
2014-07-29 22:38:01 -0700

    Added comments to logEventMatcher

--- js/logEventMatcher.js
+++ js/logEventMatcher.js
@@ -1,26 +1,37 @@
-
+/**
+ * Constructs a LogEventMatcher that can be used to match {@link LogEvent}s
+ * against the specified query
+ * 
+ * @classdesc
+ * 
+ * A LogEventMatcher is used to determine if a {@link LogEvent} matches a text
+ * query.
+ * 
+ * @constructor
+ * @param {String} query The text query
+ */
 function LogEventMatcher(query) {
-    
+
     this.query = query;
-    
+
     try {
-        
+
         this.tokenizer = new LEMTokenizer(query);
-        
+
         var parser = new LEMParser(this.tokenizer);
-        
+
         this.ast = parser.parse();
-        
+
         this.interpreter = new LEMInterpreter(this.ast);
     }
-    catch(e) {
-        if(e.constructor == Exception) {
+    catch (e) {
+        if (e.constructor == Exception) {
             e.setUserFriendly(true);
             e.append("\n");
             var querytext = query + "\n";
-            
+
             var count = this.tokenizer.getNumCharsConsumed() - 1;
-            while(count-- > 0) {
+            while (count-- > 0) {
                 querytext += " ";
             }
             querytext += "^";
@@ -28,15 +39,21 @@ function LogEventMatcher(query) {
         }
         throw e;
     }
-    
+
 }
 
+/**
+ * Returns true if the logEvent matches this' query
+ * 
+ * @param {LogEvent} logEvent The LogEvent to test
+ * @returns {Boolean} True if the log event matches
+ */
 LogEventMatcher.prototype.match = function(logEvent) {
-    try{
+    try {
         return this.interpreter.interpret(logEvent);
     }
-    catch(e) {
-        if(e.constructor == Exception) {
+    catch (e) {
+        if (e.constructor == Exception) {
             e.setUserFriendly(true);
             e.append("\n");
             e.append(this.query, "code");
@@ -45,14 +62,17 @@ LogEventMatcher.prototype.match = function(logEvent) {
     }
 };
 
+/**
+ * Returns true if any of the log events provided match this' query
+ * 
+ * @param {Array<LogEvent>} logEvents An array of log events to test
+ * @returns {Boolean} True if any of the log events match
+ */
 LogEventMatcher.prototype.matchAny = function(logEvents) {
-    for(var i = 0; i < logEvents.length; i++) {
-        if(this.match(logEvents[i])) {
+    for (var i = 0; i < logEvents.length; i++) {
+        if (this.match(logEvents[i])) {
             return true;
         }
     }
     return false;
 };
-
-
-
